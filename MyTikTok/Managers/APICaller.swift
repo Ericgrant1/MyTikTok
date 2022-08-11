b0134c62ad9c0c5042d4bcdee390903480ffdcaa
Eric Grant <senatorericgrant@gmail.com>
2022-08-11 23:59:04 +0700

    refactor: project Set Up

---
+++ MyTikTok/Managers/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  MyTikTok
+//
+//  Created by Eric Grant on 11.08.2022.
+//
+
+import Foundation
---
+++ MyTikTok/Managers/DatabaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  DatabaseManager.swift
+//  MyTikTok
+//
+//  Created by Eric Grant on 11.08.2022.
+//
+
+import Foundation
