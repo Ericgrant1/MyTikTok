f929a4c9814912351749af1236dd44408fe167bd
Eric Grant <senatorericgrant@gmail.com>
2022-08-15 22:23:39 +0700

    feat: add scrollview to HomeViewController

--- MyTikTok/Controllers/Core/HomeViewController.swift
+++ MyTikTok/Controllers/Core/HomeViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private let horizontalScroleView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.bounces = false
+        scrollView.backgroundColor = .red
+        scrollView.showsHorizontalScrollIndicator = false
+        return scrollView
+    }()
 
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        view.addSubview(horizontalScroleView)
+        setUpFeed()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        horizontalScroleView.frame = view.bounds
+        
+    }
+    
+    // Private
 
+    private func setUpFeed() {
+        
+    }
 
 }
 
